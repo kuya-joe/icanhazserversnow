ea9eab6e0f833cfb2faf10862c9307598c401132
Joe Palala <hey@kuyajoe.dev>
2021-09-29 19:26:37 +0800

    initial commit

---
+++ pages/add-swap-to-server/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="section">
+    <div class="columns is-mobile">
+      <card
+        title="Free"
+        icon="github"
+      >
+          <code>
+         <pre>
+           $  sudo fallocate -l 1G /swapfile
+           $ sudo mkswap /swapfile
+           $ sudo swapon /swapfile
+           # Check if free
+           $ free -h
+
+</pre>
+
+          </code>
+      </a>
+      </card>
+
+      <card
+        title="Responsive"
+        icon="cellphone-link"
+      >
+        <b class="has-text-grey">
+          Every
+        </b> component is responsive
+      </card>
+
+      <card
+        title="Modern"
+        icon="alert-decagram"
+      >
+        Built with <a href="https://vuejs.org/">
+          Vue.js
+        </a> and <a href="http://bulma.io/">
+          Bulma
+        </a>
+      </card>
+
+      <card
+        title="Lightweight"
+        icon="arrange-bring-to-front"
+      >
+        <NuxtLink  to="/add-swap-to-server">How to add swap space to linux server</NuxtLink>
+      </card>
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    Card
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="section">
+    <div class="columns is-mobile">
+      <card
+        title="Free"
+        icon="github"
+      >
+        Open source on <a href="https://github.com/buefy/buefy">
+          GitHub
+        </a>
+      </card>
+
+      <card
+        title="Responsive"
+        icon="cellphone-link"
+      >
+        <b class="has-text-grey">
+          Every
+        </b> component is responsive
+      </card>
+
+      <card
+        title="Modern"
+        icon="alert-decagram"
+      >
+        Built with <a href="https://vuejs.org/">
+          Vue.js
+        </a> and <a href="http://bulma.io/">
+          Bulma
+        </a>
+      </card>
+
+      <card
+        title="Lightweight"
+        icon="arrange-bring-to-front"
+      >
+        <NuxtLink  to="/add-swap-to-server">How to add swap space to linux server</NuxtLink>
+      </card>
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    Card
+  }
+}
+</script>
